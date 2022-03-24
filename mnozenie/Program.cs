5ffcfbf11c5b006ad507d1285f8b99736cbb2640
229872 <bartek.dygasinski00@gmail.com>
2022-03-24 21:06:09 +0100

    Program

---
+++ mnozenie/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Współbieżne
+{
+    public class Program
+    {
+
+        public static int multiplication(int x, int y)
+        {
+            return x * y;
+        }
+
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Program dodaje dwie liczby");
+            Console.WriteLine("Podaj pierwsza liczbe:");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj druga liczbe:");
+            int y = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Wynik: " + multiplication(x, y));
+        }
+    }
+}
